f7b88cc3fb1b4fe63da3ff8903a8d2f8f960dae8
theolenoir12 <55496601+theolenoir12@users.noreply.github.com>
2023-03-07 15:11:38 -0500

    ajout commentaires

--- extrapolation.py
+++ extrapolation.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 energie_0 = [-1.5, -0.75, -2, -1.868, -2.802, -2.855, -3.651, -3.797, -4.515, -4.718, -5.387, -5.629, -6.263, -6.533, -7.142, -7.433, -8.022, -8.33, -8.904]
-energie_1 = [0.5, 0.75, -1, -0.75, -2.118, -1.806, -3.128, -2.936, -4.092, -4, -5.031, -5, -5.956, -5.981, -6.872, -6.941, -7.781, -7.886, -8.686] #valeurs des énergies pour N=2:13
+energie_1 = [0.5, 0.75, -1, -0.75, -2.118, -1.806, -3.128, -2.936, -4.092, -4, -5.031, -5, -5.956, -5.981, -6.872, -6.941, -7.781, -7.886, -8.686] #valeurs des énergies pour N=2:20
 
 energie_0_sur_N = []
 diff_energ      = []
 
 for k in range(2,21) :
     energie_0_sur_N.append(energie_0[k-2]/k)
-    diff_energ.append(energie_1[k-2] - energie_0[k-2])
+    diff_energ.append(energie_1[k-2] - energie_0[k-2]) #on crée les liste E_0/N et E_1-E_0
 
 energie_0_sur_N_pair   = []
 energie_0_sur_N_impair = []
@@ -22,10 +22,10 @@ diff_energ_impair      = []
 for k in range(2,21) : 
     if k%2==0 :
             energie_0_sur_N_pair.append(energie_0_sur_N[k-2])
-            diff_energ_pair.append(diff_energ[k-2])
+            diff_energ_pair.append(diff_energ[k-2]) 
     else :
         energie_0_sur_N_impair.append(energie_0_sur_N[k-2])
-        diff_energ_impair.append(diff_energ[k-2])
+        diff_energ_impair.append(diff_energ[k-2]) #on crée les mêmes listes avec les termes pairs ou impairs
 
 print(epsilon(energie_0_sur_N))
 print(epsilon(energie_0_sur_N_pair))
@@ -97,4 +97,4 @@ plt.ylabel('E')
 plt.legend()
 plt.title('Convergence de la différence d énergie avec n impair')
 plt.savefig('Figures/diff_energ_impair.png')
-plt.show()
+plt.show() #on crée toutes les figures souhaitées
