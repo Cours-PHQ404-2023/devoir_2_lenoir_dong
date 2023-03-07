0e7164aed3a0a47f021c85271995c0050dc1f835
theolenoir12 <55496601+theolenoir12@users.noreply.github.com>
2023-03-07 15:09:30 -0500

    ajout commentaires

--- verif_epsilon.py
+++ verif_epsilon.py
@@ -5,7 +5,7 @@ from math import *
 serie_4 = [4, 8/3, 52/15, 304/105]
 serie_5 = serie_4 + [1052/315]
 serie_6 = serie_5 + [10312/3465]
-serie_7 = serie_6 + [147916/45045]
+serie_7 = serie_6 + [147916/45045] #les quelques séries que l'on veut tester
 
 series = [serie_4, serie_5, serie_6, serie_7]
 
